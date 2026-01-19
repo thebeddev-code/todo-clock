28667dbce76318248b87009222efa2c0b9aa447c
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-19 16:24:55 +0500

    refactor(types): move todo type to root file

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,3 +1,21 @@
 export type ClickEvent<T extends HTMLElement = HTMLElement> = MouseEvent & {
   currentTarget: T;
 };
+
+export type Todo = {
+  id: number,
+  title: string;
+  description: string;
+  tags: string[];
+  color: string | null;
+  status: "pending" | "in-progress" | "completed" | "overdue";
+  priority: "low" | "medium" | "high";
+  startsAt?: string | null;
+  due?: string | null;
+  updatedAt: string;
+  createdAt?: string;
+  completedAt?: string | null;
+  isRecurring: boolean;
+  // e.g "RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR"
+  recurrenceRule?: string | null;
+};
