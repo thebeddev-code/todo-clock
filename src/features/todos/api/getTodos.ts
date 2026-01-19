6c0b0bc759dfac08add584fd54d8ab89bb412465
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-19 16:21:43 +0500

    feat(todos): implement get todos query

---
+++ src/features/todos/api/getTodos.ts
@@ -0,0 +1,22 @@
+import { query } from "@solidjs/router";
+import { invoke } from "@tauri-apps/api/core";
+import { api } from "~/lib/client";
+import { Todo } from "~/lib/types/types";
+
+
+interface Params {
+  page?: number;
+  pageSize?: number;
+  asc?: string;
+  desc?: string;
+  due?: "today" | "tommorow" | string;
+}
+
+export const getTodos = query(async () => {
+  const todos = (await api.get("todos")) as Todo[]
+  return todos;
+}, "todos");
+
+
+
+
