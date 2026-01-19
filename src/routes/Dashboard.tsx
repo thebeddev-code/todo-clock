1956ee692c3e7927f70b492bd265bb3592d3842d
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-01-19 17:03:38 +0500

    feat(dashboard): integrate todos fetching

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,10 +3,13 @@ import { DayVisualizer } from "~/features/visualizer/components/DayVisualizer";
 // import { TodoFormWrapper } from "@/features/todos/TodoFormWrapper";
 // import { TodoList } from "@/features/todos/TodoList";
 import { addDays, set } from "date-fns";
-import { createSignal } from "solid-js";
-import { Todo } from "~/lib/types/api";
+import { createSignal, Show } from "solid-js";
+import { Todo } from "~/lib/types";
+import { createAsync } from "@solidjs/router";
+import { getTodos } from "~/features/todos/api/getTodos";
 
 export default function Dashboard() {
+  const todos = createAsync(() => getTodos())
   const [currentDate, setCurrentDate] = createSignal(new Date());
   // const { data, status } = useTodos({
   //   params: {
@@ -14,29 +17,32 @@ export default function Dashboard() {
   //   },
   // });
   // const changeFormType = useTodoForm((state) => state.changeFormType);
-  const todos: Todo[] = [];
   return (
     <main class="flex justify-center items-center h-dvh flex-1 _grid _grid-cols-2">
       {/* {status === "success" && todos && ( */}
-      <DayVisualizer
-        todos={todos}
-        currentDate={currentDate()}
-        onMoveDate={(days) => {
-          let date = set(currentDate(), {
-            hours: 0,
-            minutes: 5,
-            seconds: 0,
-          });
-          if (days < 0) {
-            date = set(currentDate(), {
-              hours: 23,
-              minutes: 55,
+      <Show when={todos()}>
+        <DayVisualizer
+          todos={todos() as Todo[]}
+          currentDate={currentDate()}
+          onMoveDate={(days) => {
+            let date = set(currentDate(), {
+              hours: 0,
+              minutes: 5,
               seconds: 0,
             });
-          }
-          setCurrentDate(addDays(date, days));
-        }}
-      />
+            if (days < 0) {
+              date = set(currentDate(), {
+                hours: 23,
+                minutes: 55,
+                seconds: 0,
+              });
+            }
+            setCurrentDate(addDays(date, days));
+          }}
+        />
+
+      </Show>
+
       <div></div>
       {/* )} */}
       {/* {status === "success" && todos && <TodoList todos={todos} />} */}
