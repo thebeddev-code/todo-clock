347bfd3d05e6c45a0451fdfcc2fa4c612a7e8eb2
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-01 18:57:03 +0500

    feat(todos): revalidate queries on create, delete

--- frontend/src/features/todos/api/createTodo.ts
+++ frontend/src/features/todos/api/createTodo.ts
@@ -1,5 +1,7 @@
+import { revalidate } from "@solidjs/router";
 import { api, createTypedQuery } from "~/lib/client";
 
-export const createTodo = createTypedQuery<"/todos", "POST">((options) => {
-  return api.create("/todos", options)
+export const createTodo = createTypedQuery<"/todos", "POST">(async (options) => {
+  await api.create("/todos", options)
+  revalidate("/todos")
 }, "/todos")
--- frontend/src/features/todos/api/deleteTodo.ts
+++ frontend/src/features/todos/api/deleteTodo.ts
@@ -1,5 +1,7 @@
+import { revalidate } from "@solidjs/router";
 import { api, createTypedQuery } from "~/lib/client";
 
-export const deleteTodoMutation = createTypedQuery<"/todos", "DELETE">((options) => {
-  return api.delete("/todos", options)
+export const deleteTodoMutation = createTypedQuery<"/todos", "DELETE">(async (options) => {
+  await api.delete("/todos", options)
+  revalidate("/todos")
 }, "/todos")
