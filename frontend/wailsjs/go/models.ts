095cb9b7982eec97026b95fa901d7bf5975b799e
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-01 18:14:21 +0500

    fix(backend): update todo model and regenerate
    - Removed duplicate fields
    - Made ID field lower case

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,13 +1,7 @@
 export namespace services {
 	
 	export class Todo {
-	    ID: number;
-	    // Go type: time
-	    CreatedAt: any;
-	    // Go type: time
-	    UpdatedAt: any;
-	    // Go type: gorm
-	    DeletedAt: any;
+	    id: number;
 	    title: string;
 	    description?: string;
 	    tags: string[];
@@ -28,10 +22,7 @@ export namespace services {
 	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.ID = source["ID"];
-	        this.CreatedAt = this.convertValues(source["CreatedAt"], null);
-	        this.UpdatedAt = this.convertValues(source["UpdatedAt"], null);
-	        this.DeletedAt = this.convertValues(source["DeletedAt"], null);
+	        this.id = source["id"];
 	        this.title = source["title"];
 	        this.description = source["description"];
 	        this.tags = source["tags"];
@@ -46,24 +37,6 @@ export namespace services {
 	        this.isRecurring = source["isRecurring"];
 	        this.recurrenceRule = source["recurrenceRule"];
 	    }
-	
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
 	}
 
 }
--- services/todos.go
+++ services/todos.go
@@ -1,14 +1,11 @@
 package services
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Todo struct {
-	gorm.Model
-	// ID             int      `json:"id" gorm:"primaryKey"`
+	ID             int      `json:"id" gorm:"primaryKey"`
 	Title          string   `json:"title"`
 	Description    *string  `json:"description"`
 	Tags           []string `json:"tags" gorm:"serializer:json"`
@@ -35,29 +32,7 @@ func NewTodoService(db *gorm.DB) *TodoService {
 
 func (s *TodoService) GetTodos() ([]Todo, error) {
 	var todos []Todo
-	now := time.Now().UTC()
-	isoString := now.Format(time.RFC3339)
-	color := "blue"
-	rule := "RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR"
-	description := "Study Go language and build a project"
-	mockTodo := Todo{
-		// ID:             1,
-		Title:          "Learn Go",
-		Description:    &description,
-		Tags:           []string{"study", "go"},
-		Color:          &color,
-		Status:         "in-progress",
-		Priority:       "high",
-		StartsAt:       &isoString,
-		Due:            &isoString,
-		UpdatedAt:      &isoString,
-		CreatedAt:      &isoString,
-		CompletedAt:    nil,
-		IsRecurring:    true,
-		RecurrenceRule: &rule,
-	}
 	result := s.db.Find(&todos)
-	todos = append(todos, mockTodo)
 	return todos, result.Error
 }
 
