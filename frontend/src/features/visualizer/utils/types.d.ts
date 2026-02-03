7f4a0f27ab42fa707067e33e6f7cf877c587ac6e
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-03 17:14:49 +0500

    feat(visualizer): add VisualizableTodo type
    This type consists of todo time fields [startsAt, due] and color field

--- frontend/src/features/visualizer/utils/types.d.ts
+++ frontend/src/features/visualizer/utils/types.d.ts
@@ -1,3 +1,7 @@
+import { Todo } from "~/lib/types";
+
+export type VisualizableTodo = Pick<Todo, "startsAt" | "due" | "color">
+
 export type DrawableTodo = {
   startTimeHours: number;
   endTimeHours: number;
