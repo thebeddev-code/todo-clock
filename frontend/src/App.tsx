25124e0bdccbd249917f86a8e5e761992be94afc
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-04 17:15:18 +0500

    refactor(frontend): rm tauri import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/core";
 
-function App() {
-	return (
+function App() {return (
 		<main class="container">
 			<div class="w-10 bg-black" />
 		</main>
