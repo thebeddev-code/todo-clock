8ccab27aa9c8b1ae0505e993c62b3792d40edb22
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-04 19:14:51 +0500

    refactor(client): pass typed api client to query handler

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,50 +1,35 @@
 import { query } from "@solidjs/router";
+import type { EndpointOptions, EndpointResponse, Endpoints } from "./router";
 import * as backend from "./router";
-import type { EndpointOptions, Endpoints, EndpointResponse } from "./router";
 
 const { routerConfig } = backend;
 
-class ApiClient {
-	constructor() {}
+class ApiClient<
+	E extends Endpoints,
+	M extends keyof (typeof backend.routerConfig)[E],
+> {
 	private async request<T>(
 		method: string,
 		route: string,
 		options = {},
 	): Promise<T> {
-		if (true) {
-			return backend.handleRequest(method, route, options) as T;
-		}
+		return backend.handleRequest(method, route, options) as T;
 		// TODO: implement web client logic
-		// throw new Error("Not yet implemented!")
-		console.error("Web not implemented yet!");
-		return [] as T;
 	}
 
-	async get<
-		E extends Endpoints,
-		M extends keyof (typeof backend.routerConfig)[E],
-	>(url: string, options?: EndpointOptions<E, M>) {
+	async get(url: string, options?: EndpointOptions<E, M>) {
 		return this.request("GET", url, options);
 	}
 
-	async create<
-		E extends Endpoints,
-		M extends keyof (typeof backend.routerConfig)[E],
-	>(url: string, options: EndpointOptions<E, M>) {
+	async create(url: string, options: EndpointOptions<E, M>) {
 		return this.request("POST", url, options);
 	}
 
-	async update<
-		E extends Endpoints,
-		M extends keyof (typeof backend.routerConfig)[E],
-	>(url: string, options?: EndpointOptions<E, M>) {
+	async update(url: string, options?: EndpointOptions<E, M>) {
 		return this.request("PATCH", url, options);
 	}
 
-	async delete<
-		E extends Endpoints,
-		M extends keyof (typeof backend.routerConfig)[E],
-	>(url: string, options?: EndpointOptions<E, M>) {
+	async delete(url: string, options?: EndpointOptions<E, M>) {
 		return this.request("DELETE", url, options);
 	}
 }
@@ -56,12 +41,15 @@ export function createTypedQuery<
 	E extends Endpoints,
 	M extends keyof (typeof routerConfig)[E],
 >(
-	handler: (options: EndpointOptions<E, M>) => Promise<unknown>,
+	handler: (
+		options: EndpointOptions<E, M>,
+		typedApiClient: ApiClient<E, M>,
+	) => Promise<unknown>,
 	route: E | string,
 ) {
 	return query(
 		(options: EndpointOptions<E, M>) =>
-			handler(options) as EndpointResponse<E, M>,
+			handler(options, api) as EndpointResponse<E, M>,
 		route,
 	);
 }
