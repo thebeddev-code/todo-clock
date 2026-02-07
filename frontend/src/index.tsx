028a0d16812c5f3d5b7c633e42bfc92e549ecf08
thebeddev-code <240914167+thebeddev-code@users.noreply.github.com>
2026-02-08 00:51:09 +0500

    feat(main): add theme persistence and update imports
    - Apply theme on app load
    - Updated imports to point to new routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,18 @@
 import { Route, Router } from "@solidjs/router";
 import { render } from "solid-js/web";
 import "./index.css";
-import Dashboard from "./routes/Dashboard.tsx";
-import DashboardLayout from "./routes/dashboard/layout.tsx";
+import { applyTheme } from "./lib/utils/theme.ts";
+import Activities from "./routes/activities.tsx";
+import DashboardLayout from "./routes/layout.tsx";
+import Settings from "./routes/settings.tsx";
 
+applyTheme();
 render(
 	() => (
 		<Router root={DashboardLayout}>
-			<Route path="/" component={Dashboard} />
+			<Route path="/" component={Activities} />
 			<Route path="/calendar" />
+			<Route path="/settings" component={Settings} />
 		</Router>
 	),
 	document.getElementById("root") as HTMLElement,
